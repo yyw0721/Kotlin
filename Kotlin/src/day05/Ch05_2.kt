bd14b10de11b4ad7f89a7115c2d66a395908a2bc
yyw0721 <yyw0721@gmail.com>
2021-05-14 11:09:58 +0900

    commit

---
+++ Kotlin/src/day05/Ch05_2.kt
@@ -0,0 +1,27 @@
+package day05
+
+class Ch05_2 {
+    var name: String
+    var wing: Int
+    var beak: String
+    var color: String
+
+    constructor(name: String, wing: Int, beak: String, color:String){
+        this.name = name
+        this.wing = wing
+        this.beak = beak
+        this.color = color
+    }
+
+    fun fly() = println("Fly wing: ${wing}")
+    fun sing(vol: Int) = println("Sing vol: ${vol}")
+}
+
+fun main(){
+    val coco = Ch05_2("myBird", 2, "short", "blue")
+
+    coco.color = "yellow"
+    println("coco.color: ${coco.color}")
+    coco.fly()
+    coco.sing(3)
+}
